8596c0179d5f8371db9e7aa279458456a54c8814
Aleksei Rybin <aleksei@tapni.su>
2025-09-09 23:15:54 +0700

    add groups list fetcher

---
+++ src/lists/groups.rs
@@ -0,0 +1,28 @@
+use serde::Deserialize;
+
+#[derive(Deserialize, Default, Debug, Clone, PartialEq, Eq, Hash)]
+pub struct GroupsList {
+    #[serde(rename = "GroupName")]
+    pub data: Vec<GroupsListItem>,
+    pub success: bool,
+}
+
+#[derive(Deserialize, Default, Debug, Clone, PartialEq, Eq, Hash)]
+pub struct GroupsListItem {
+    #[serde(rename = "GroupName")]
+    pub group_name: String,
+    #[serde(rename = "FacultyName")]
+    pub faculty_name: String,
+}
+
+impl GroupsList {
+    pub async fn fetch() -> reqwest::Result<Vec<GroupsListItem>> {
+        let groups_list = reqwest::get("https://rasps.nsuem.ru/data/groups")
+            .await?
+            .json::<GroupsList>()
+            .await?
+            .data;
+
+        Ok(groups_list)
+    }
+}
